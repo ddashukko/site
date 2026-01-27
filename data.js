765b982f5cefbdbfe5a772d015b2008edc2b8197
ddashukko <dasha.kerroll@gmail.com>
2026-01-28 01:34:17 +0200

    Update data.js

--- data.js
+++ data.js
@@ -763,6 +763,16 @@ window.lessonsData = [
     author: "Соня",
   },
   {
+    id: "math-6-riznidii",
+    title: "Тест, різні знаки, різні дії",
+    grade: 6,
+    topic: "Математика",
+    link: "math_general/6/Вадим/math_6_rizniznaki_riznidii.html",
+    subject_code: "math_general",
+    category: "lesson",
+    author: "Соня",
+  },
+  {
     id: "math-6-vadim-03",
     title: "Вадим Урок3",
     grade: 6,
